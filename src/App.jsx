f0b7d26164f4b601613af0c2935e7b2020f8ac9f
sameerjohn1 <sameerprogrammer5@gmail.com>
2024-07-10 12:36:33 +0500

    add 3d iphone web

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useRef } from "react";
+import DisplaySection from "./components/DisplaySection";
+import Jumbotron from "./components/Jumbotron";
+import Nav from "./components/Nav";
+import SoundSection from "./components/SoundSection";
+import WebgiWiewer from "./components/WebgiWiewer";
+import Loader from "./components/Loader";
+
+function App() {
+  const webgiViewerRef = useRef();
+  const contentRef = useRef();
+
+  const handlePreview = () => {
+    webgiViewerRef.current.triggerPreview();
+  };
+
+  return (
+    <div className="App">
+      <Loader />
+      <div ref={contentRef} id="content">
+        <Nav />
+        <Jumbotron />
+        <SoundSection />
+        <DisplaySection triggerPreview={handlePreview} />
+      </div>
+
+      <WebgiWiewer contentRef={contentRef} ref={webgiViewerRef} />
+    </div>
+  );
+}
+
+export default App;
+// 40:10
